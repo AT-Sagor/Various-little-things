f72b064298ca0ece8d62fdb52e0d11facbd580d4
AT_Sagor <47899697+AT-Sagor@users.noreply.github.com>
2021-01-05 16:19:17 +0400

    Update script.js

--- canvas - постепенное ресование прямоугольников/script.js
+++ canvas - постепенное ресование прямоугольников/script.js
@@ -1,26 +1,29 @@
 var windowHeight = window.innerHeight - 20;
 var windowWidth = window.innerWidth - 20;
-var iTimeOut =  0;
+var iCanvas = 0;
 
 document.body.innerHTML += '<canvas class="smile" width="'+windowWidth+'" height="'+windowHeight+'"></canvas>';
 
-for (iCanvas = 0; iCanvas <= 200; iCanvas++) {
- 
- function cavasCru () { 
-  var randomНeight2 = Math.round(Math.random()*300);
-  var randomWidth2 = Math.round(Math.random()*300);
-  var randomНeight = Math.round(Math.random()*windowHeight - randomНeight2);
-  var randomWidth = Math.round(Math.random()*windowWidth - randomWidth2);
+var drawingCanvas = document.getElementsByClassName("smile")[0];
+var context = drawingCanvas.getContext('2d');
 
-  var iColor1 = Math.round(Math.random()*256);
-  var iColor2 = Math.round(Math.random()*256);
-  var iColor3 = Math.round(Math.random()*256);
-  let drawingCanvas = document.getElementsByClassName("smile")[0];
-  let context = drawingCanvas.getContext('2d');
+function cavasCru() {
+  let randomНeight2 = Math.round(Math.random()*300);
+  let randomWidth2 = Math.round(Math.random()*300);
+  let randomНeight = Math.round(Math.random()*windowHeight - randomНeight2);
+  let randomWidth = Math.round(Math.random()*windowWidth - randomWidth2);
+  let iColor1 = Math.round(Math.random()*256);
+  let iColor2 = Math.round(Math.random()*256);
+  let iColor3 = Math.round(Math.random()*256);
+ 
   context.fillStyle = 'rgba('+iColor1+', '+iColor2+', '+iColor3+', 0.8)';
   context.fillRect(randomWidth,randomНeight,randomНeight2,randomWidth2);
+  iCanvas++;
+  
+  if (iCanvas >= 200) {
+   context.clearRect(0, 0, windowWidth, windowHeight);
+   iCanvas = 0;
+  }
  }
  
- setTimeout(cavasCru, iTimeOut);
- iTimeOut = iTimeOut + 50;
-}
+ setInterval(cavasCru, 10);
