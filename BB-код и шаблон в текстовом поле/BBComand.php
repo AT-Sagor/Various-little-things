89797cad713fb2407bf3da3da7d7fbb6eb0b371b
AT_Sagor <47899697+AT-Sagor@users.noreply.github.com>
2022-06-27 19:08:11 +0400

    Update BBComand.php

--- BB-код и шаблон в текстовом поле/BBComand.php
+++ BB-код и шаблон в текстовом поле/BBComand.php
@@ -1,9 +1,13 @@
 <?php
  $aBBComand = array (
   '/\[b\](.*?)\[\/b\]/is',
+  '/\[i\](.*?)\[\/i\]/is',
+  '/\[u\](.*?)\[\/u\]/is',
  );
  
  $aBBPerformance = array (
   '<strong>$1</strong>',
+  '<i>$1</i>',
+  '<u>$1</u>',
  );
 ?>
