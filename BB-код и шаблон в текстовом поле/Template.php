d143acd2f1855d4a8a724cfe8a76f92f538639f7
AT_Sagor <47899697+AT-Sagor@users.noreply.github.com>
2022-06-27 19:10:28 +0400

    Update Template.php

--- BB-код и шаблон в текстовом поле/Template.php
+++ BB-код и шаблон в текстовом поле/Template.php
@@ -2,10 +2,14 @@
  $aTemplate = array (
   '/\{(test1)\}/is',
   '/\{(test2)\}/is',
+  '/\{(random1_100)\}/is',
  );
  
  $aTemplatePerformance = array (
   '<strong>Выполнение команды №1</strong>',
   '<i>Выполнение команды №2</i>',
+  '$iRandom1_100;',
  );
+ 
+ $aTemplatePerformance[2] = rand(1, 100);
 ?>
